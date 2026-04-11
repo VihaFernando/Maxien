a9522dd799945dcc990f35f55a108de2aaf097a4
Kavindu Thihansa <kavinduashaingo@gmail.com>
2026-04-11 23:15:28 +0530

    Update LifeSyncManga.jsx

--- src/pages/lifesync/LifeSyncManga.jsx
+++ src/pages/lifesync/LifeSyncManga.jsx
@@ -9,7 +9,6 @@ import {
     LifesyncMangaBrowseGridSkeleton,
     LifesyncMangaChapterListSkeleton,
 } from '../../components/lifesync/EpisodeLoadingSkeletons'
-import { FadeInImg } from '../../components/lifesync/FadeInImg'
 import { LifeSyncSectionNav } from '../../components/lifesync/LifeSyncSectionNav'
 import {
     AnimatePresence,
@@ -863,7 +862,7 @@ function MangaDetail({ manga, onClose, source, onStartRead, mangadexConnected, b
                                 style={{ aspectRatio: '2/3' }}
                             >
                                 {coverImg ? (
-                                    <FadeInImg src={coverImg} alt="" className="h-full w-full object-cover" {...mangadexImageProps(coverImg)} />
+                                    <img src={coverImg} alt="" className="h-full w-full object-cover" {...mangadexImageProps(coverImg)} />
                                 ) : (
                                     <div className="flex h-full min-h-[7.5rem] w-full items-center justify-center">
                                         <svg className="h-10 w-10 text-[#86868b]" fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth="1.5"><path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" /></svg>
